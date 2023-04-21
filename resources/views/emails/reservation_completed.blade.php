2fbe7e6c66e38f2fc81a682f13b5082bd9ee6653
ryubastard <yokoyama.linkh@gmail.com>
2023-04-21 23:23:21 +0900

    メールデザイン

--- resources/views/emails/reservation_completed.blade.php
+++ resources/views/emails/reservation_completed.blade.php
@@ -1,25 +1,54 @@
-    <p>{{ $user }}様</p>
-    <p>この度は、レッスンの予約が完了しました。</p>
-    <p>以下の詳細情報をご確認ください。</p>
-    <table>
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="width: 100%;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+    <table width="100%" style="border-collapse: collapse;border-spacing: 0;border: 0 none;vertical-align: top;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #3B5154;background: #ECEFF1;border-top-color: #64FFDA;border-top-width: 5px;border-top-style: solid;text-align: center">
         <tr>
-            <td>レッスン名:</td>
-            <td>{{ $lesson->name }}</td>
-        </tr>
-        <tr>
-            <td>レッスン料金:</td>
-            <td>{{ $lesson->price }}</td>
-        </tr>
-        <tr>
-            <td>レッスン場所:</td>
-            <td>{{ $lesson->location }}</td>
-        </tr>
-        <tr>
-            <td>開始時間:</td>
-            <td>{{ $lesson->start_date }}</td>
-        </tr>
-        <tr>
-            <td>終了時間:</td>
-            <td>{{ $lesson->end_date }}</td>
+            <td style="padding: 28px 0">
+                <h1 style="margin: 0;padding: 0;font-size: 34.18px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;margin-bottom: 14px">レッスン予約完了通知</h1>
+            </td>
         </tr>
     </table>
+
+    <table class="h-align--center" width="100%" style="border-collapse: collapse;border-spacing: 0;border: 0 none;vertical-align: top;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;text-align: center">
+        <tr style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+            <td style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+                <table class="c-responsive_container" style="margin: 0 auto;border-collapse: collapse;border-spacing: 0;border: 0 none;vertical-align: top;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;width: 95%;max-width: 640px">
+                    <tr style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+                        <td class="h-align--left h-padding_double--top" style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;padding-top: 28px;text-align: left">
+                            <h2 style="margin: 0;padding: 0;font-size: 21.875px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;margin-bottom: 14px">
+                                ・ご予約内容</h2>
+                            <p style="margin: 0;padding: 0;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;display: block;margin-bottom: 14px">
+                                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                            </p>
+                            <p style="margin: 0;padding: 0;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;display: block;margin-bottom: 14px">
+                                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。
+                            </p>
+                        </td>
+                    </tr>
+                    <tr style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+                        <td class="h-align--center" style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;text-align: center">
+                            <p style="margin: 0;padding: 0;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;display: block;margin-bottom: 14px">
+                            </p>
+                            <p class="c-block h-align--center" style="margin: 0;padding: 0;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;display: block;margin-bottom: 14px;text-align: center">
+                                <a class="c-btn" href="#" style="margin: 0 1em;padding: 1em 4em;font-size: 1.2em;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #00523F;text-decoration: none;border-radius: 2px;display: inline-block;background-color: #64FFDA">ボタン</a>
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+    <table class="footer" width="100%" style="border-collapse: collapse;border-spacing: 0;border: 0 none;vertical-align: top;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #414C58;background-color: #B0BEC5;text-align: center">
+        <tr style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+            <td style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+                <table class="c-responsive_container" style="margin: 0 auto;border-collapse: collapse;border-spacing: 0;border: 0 none;vertical-align: top;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;width: 95%;max-width: 640px">
+                    <tr style="font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959">
+                        <td class="h-align--left h-text--small h-padding_double--top h-padding--bottom" style="font-size: 12px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;padding-bottom: 14px;padding-top: 28px;text-align: left">
+                            <p class="h-align--center" style="margin: 0;padding: 0;font-size: 14px;font-family: Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Osaka, sans-serif;color: #595959;display: block;margin-bottom: 14px;text-align: center">
+                                © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.') </p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
